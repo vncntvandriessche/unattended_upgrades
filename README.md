e9d2a8eebbe17419532f64642b691315f1c6f0e1
Vincent Van Driessche <vncntvandriessche@gmail.com>
2014-10-21 21:35:24 +0200

    Initializes the cookbook

---
+++ README.md
@@ -0,0 +1,4 @@
+# unattended_upgrades_
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'unattended_upgrades'
+maintainer 'Vincent Van Driessche'
+maintainer_email 'vncntvandriessche@gmail.com'
+license 'MIT'
+description 'Installs/Configures unattended_upgrades'
+long_description 'Installs/Configures unattended_upgrades'
+version '0.1.0'
+
+depends 'apt'
